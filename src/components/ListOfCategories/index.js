b05654155dac91bde484a9965eabb10d4bb50d0a
MariaRoblesSpin <maria.robles@spinmedia.es>
2019-08-19 12:33:31 +0200

    now configuration

---
+++ src/components/ListOfCategories/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import { Category } from '../Category'
+import { List, Item } from './styles'
+import { categories as mockCategories } from '../../../api/db.json'
+export const ListOfCategories = () => {
+  const [categories, setCategories] = useState(mockCategories)
+  return (
+    <List>
+      {
+        categories.map(category => <Item key={category}><Category {...category} /></Item>)
+      }
+    </List>
+  )
+}
---
+++ src/components/PhotoCard/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ImgWrapper, Img, Button } from './styles'
+import { MdFavoriteBorder } from 'react-icons/md'
+const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1425082661705-1834bfd09dca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  return (
+    <article>
+      <a href={`/detail/${id}`}>
+        <ImgWrapper>
+          <Img src={src} />
+        </ImgWrapper>
+      </a>
+      <Button>
+        <MdFavoriteBorder size='32px' /> {likes} likes!
+      </Button>
+    </article>
+  )
+}
